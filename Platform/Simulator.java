b54b58046984d6225c48aa4fc37e1720d2f48024
Mingcheng Chen <linyufly@linyufly-desktop.(none)>
2014-09-10 00:55:49 -0500

    Added Simulator.java

--- Platform/Simulator.java
+++ Platform/Simulator.java
@@ -2,6 +2,11 @@
 
 public class Simulator {
   public static void main(String[] args) {
+    if (args.length != 6) {
+      System.println("Usage: java Simulator <world> <agent> <knows_thief> <steps> <episodes> <policy_output>");
+      return;
+    }
+
     String worldClassName = args[0];
     String agentClassName = args[1];
     int numOfEpisodes = Integer.parseInt(args[2]);
